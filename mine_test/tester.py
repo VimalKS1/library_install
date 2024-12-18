e30ec5f6edb0e3167bea438176658c068195f1c9
K Vimal <k.vimal@bizmetric.com>
2024-12-18 10:55:33 +0530

    Initial commit

---
+++ mine_test/tester.py
@@ -0,0 +1,4 @@
+import calculation_mine_library 
+print(calculation_mine_library.sum(1,1))
+
+print(calculation_mine_library.sub(10,4))
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calulation_mine_library",  # Name of the library
+    version="0.1",  # Version number
+     packages=find_packages(),  
+   
+    url="https://github.com/VimalKS1/Python_projects",  
+    
+)
